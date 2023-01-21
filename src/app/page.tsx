09cff2bbca4ce154d9aee6642f987a3d4c90c849
clbertolini <carterbertolini@gmail.com>
2023-01-21 16:17:09 -0500

    Added Inter font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
   return (
     <p className="font-sans hover:font-bold">Hello, World!</p>
