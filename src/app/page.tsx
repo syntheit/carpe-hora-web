125d5c9275e5558abffbd4ae3d67ffb2b60bd2ee
syntheit <daniel@matv.io>
2023-01-22 23:34:19 -0500

    Made changes that I forgot what were bceause I was driving for like three and a half hours

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-'use client'
+"use client";
 
-import Timeline from '@/components/Dashboard/Timeline'
-import { Event } from '@/components/Dashboard/TimelineEntry';
-import ActivityAdder from '@/components/Dashboard/ActivityAdder';
-import { auth } from '@/constants/firebase';
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
+import Timeline from "@/components/Dashboard/Timeline";
+import { Event } from "@/components/Dashboard/TimelineEntry";
+import ActivityAdder from "@/components/Dashboard/ActivityAdder";
+import { auth } from "@/constants/firebase";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Dashboard() {
   const [update, setUpdate] = useState(false);
@@ -19,15 +19,15 @@ export default function Dashboard() {
   return (
     <div className="flex gap-10 h-full w-screen px-10">
       <div className="bg-black flex-initial w-1/4 rounded-3xl box_shadow">
-        <Timeline count={count}/>
+        <Timeline count={count} />
       </div>
       <div className="flex flex-col gap-10 grow">
         <div className="flex flex-row gap-10 flex-initial h-[30vh]">
-          <ActivityAdder update={setUpdate}/>
+          <ActivityAdder update={setUpdate} />
           <div className="invisible flex-initial w-[30vh]" />
         </div>
         <div className="bg-black grow rounded-3xl box_shadow">
-          Two Week Overview
+          <h2 className="p-5 font-semibold text-[50px]">Two Week Overview</h2>
         </div>
       </div>
     </div>
--- src/app/statistics/page.tsx
+++ src/app/statistics/page.tsx
@@ -1,64 +1,160 @@
 "use client";
 
-import {
-  query,
-  orderBy,
-  limit,
-  collection,
-  getDocs,
-  QuerySnapshot,
-  DocumentData,
-} from "firebase/firestore";
+import { query, limit, collection, getDocs } from "firebase/firestore";
 import { db, auth } from "@/constants/firebase";
-import { Doughnut } from "react-chartjs-2";
-import { useEffect } from "react";
+import { Doughnut, Line } from "react-chartjs-2";
+import { useLayoutEffect } from "react";
+import {
+  ArcElement,
+  CategoryScale,
+  Chart,
+  Legend,
+  LinearScale,
+  LineElement,
+  PointElement,
+  Title,
+  Tooltip,
+} from "chart.js";
+import { faker } from "@faker-js/faker";
 
-export default async function Statistics() {
-  // const daysRef = collection(db, `users/${auth.currentUser?.uid}/days`);
-  // const q = query(daysRef, orderBy("name"), limit(14));
-  let activitiesArray: number[] = new Array(23);
-  // useEffect(() => {
-  //   // getDocs(q).then((querySnapshot) => {
-  //   //   querySnapshot.forEach((doc) => {
-  //   //     console.log(doc.data());
-  //   //     // activitiesarray.push(doc.data());
-  //   //   });
-  //   // });
-  // });
-  // console.log(activitiesArray);
-  // const doughnutValues = {
-  //   labels: ["Sleep", "Work", "Exercise", "Eat", "Socialize", "Relax", "Other"],
-  //   datasets: [
-  //     {
-  //       label: "Instances",
-  //       data: activitiesArray,
-  //       backgroundColor: [
-  //         "#f5222d",
-  //         "#fa541c",
-  //         "#fa8c16",
-  //         "#fadb14",
-  //         "#52c41a",
-  //         "#13c2c2",
-  //         "#1677ff",
-  //         "#722ed1",
-  //         "#eb2f96",
-  //         "#00bfa5",
-  //         "#009e60",
-  //       ],
-  //     },
-  //   ],
-  // };
+Chart.register(
+  ArcElement,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+let activitiesCount = [0, 0, 0, 0, 0, 0, 0];
+
+const labels = [
+  "Sleep",
+  "Work",
+  "Exercise",
+  "Eat",
+  "Socialize",
+  "Relax",
+  "Other",
+];
+
+export default function Statistics() {
+  let doughnutValues = {
+    labels,
+    datasets: [
+      {
+        label: "Instances",
+        data: [],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const doughnutOptions = {
+    responsive: true,
+  };
+
+  const trendOptions = {
+    maintainAspectRatio: false,
+  };
+
+  let trendValues = {
+    labels,
+    datasets: [
+      {
+        label: "Sleep",
+        data: labels.map(() => faker.datatype.number({ min: 0, max: 100 })),
+        borderColor: "rgb(0, 255, 132)",
+        backgroundColor: "rgba(0, 255, 132, 0.5)",
+      },
+      {
+        label: "Work",
+        data: labels.map(() => faker.datatype.number({ min: 0, max: 100 })),
+        borderColor: "rgb(53, 162, 235)",
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
+      },
+      {
+        label: "Exercise",
+        data: labels.map(() => faker.datatype.number({ min: 0, max: 100 })),
+        borderColor: "rgba(255, 99, 132, 1)",
+        backgroundColor: "rgba(255, 99, 132, 0.2)",
+      },
+      {
+        label: "Eat",
+        data: labels.map(() => faker.datatype.number({ min: 0, max: 100 })),
+        borderColor: "rgba(50, 162, 190, 1)",
+        backgroundColor: "rgba(50, 162, 190, 0.2)",
+      },
+      {
+        label: "Socialize",
+        data: labels.map(() => faker.datatype.number({ min: 0, max: 100 })),
+        borderColor: "rgba(255, 206, 86, 1)",
+        backgroundColor: "rgba(255, 206, 86, 0.2)",
+      },
+      {
+        label: "Relax",
+        data: labels.map(() => faker.datatype.number({ min: 0, max: 100 })),
+        borderColor: "rgba(75, 192, 192, 1)",
+        backgroundColor: "rgba(153, 102, 255, 0.2)",
+      },
+      {
+        label: "Other",
+        data: labels.map(() => faker.datatype.number({ min: 0, max: 100 })),
+        borderColor: "rgba(255, 159, 64, 1)",
+        backgroundColor: "rgba(255, 159, 64, 0.2)",
+      },
+    ],
+  };
+
+  useLayoutEffect(() => {
+    const daysRef = collection(db, `users/${auth.currentUser?.uid}/days`);
+    const q = query(daysRef, limit(14));
+    getDocs(q).then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        doc.data().hours.forEach((activity: number, i: number) => {
+          activitiesCount[activity]++;
+        });
+      });
+    });
+    doughnutValues.datasets[0].data = activitiesCount as never[];
+  }, [doughnutValues.datasets]);
+
   return (
     <div className="flex gap-10 h-full w-screen px-10">
       <div className="container box_shadow flex-initial w-1/4 "></div>
       <div className="flex flex-col gap-10 grow">
         <div className="flex flex-row gap-10 flex-initial h-[30vh]">
-          <div className="bg-black grow rounded-[20px]">Record Activity</div>
-          <div className="invisible flex-initial w-[30vh]" />
+          <div className="grow container p-8 w-5/12">
+            <h1 className="text-3xl font-bold">Trends</h1>
+            <Line data={trendValues} options={trendOptions} />
+          </div>
+          <div className="invisible flex-initial w-[40vh]" />
         </div>
         <div className="container grow p-8">
           <h3 className="text-3xl font-bold">Past Two Weeks</h3>
-          {/* <Doughnut data={doughnutValues} /> */}
+          <div className="w-2/6 h-full">
+            <Doughnut data={doughnutValues} options={doughnutOptions} />
+          </div>
         </div>
       </div>
     </div>
